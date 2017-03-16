f7ce8d7895099833f70ec0ae98e5c9ffd49c72be
Brian Marx <b.marx981@gmail.com>
2017-03-16 15:32:33 -0400

    initial commit Just the storyboard is put together.

---
+++ PhotoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoViewController.swift
+//  Photorama
+//
+//  Created by Marx, Brian on 3/16/17.
+//  Copyright © 2017 Marx, Brian. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
