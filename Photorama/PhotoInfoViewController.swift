14d564bd7ae04cb422309fc93d7467f6af1427d3
Brian Marx <b.marx981@gmail.com>
2017-03-23 14:49:35 -0400

    View controller for the pictures layout shows up

---
+++ Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Marx, Brian on 3/23/17.
+//  Copyright © 2017 Marx, Brian. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { (result) -> Void in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
