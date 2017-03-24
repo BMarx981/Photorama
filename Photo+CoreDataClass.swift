66ec244b8029791af7ac0b0587ef27e3bd8f077e
Brian Marx <b.marx981@gmail.com>
2017-03-24 09:55:12 -0400

    Setup Core data

---
+++ Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Marx, Brian on 3/23/17.
+//  Copyright © 2017 Marx, Brian. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -64,7 +64,9 @@ class PhotoStore {
     
     func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
         
-        let photoKey = photo.photoID
+        guard let photoKey = photo.photoID else {
+            preconditionFailure("Photo expected to have a photoID.")
+        }
         if let image = imageStore.image(forKey: photoKey) {
             OperationQueue.main.addOperation {
                 completion(.success(image))
@@ -72,8 +74,10 @@ class PhotoStore {
             return
         }
         
-        let photoURL = photo.remoteURL
-        let request = URLRequest(url: photoURL)
+        guard let photoURL = photo.remoteURL else {
+            preconditionFailure("Photo expected to have remote URL")
+        }
+        let request = URLRequest(url: photoURL as URL)
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
